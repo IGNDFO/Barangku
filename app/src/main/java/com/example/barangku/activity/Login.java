0e79a9c543b412b96de8b8cf0d3aa269e6bad214
fernando <fernand0753@mhs.mdp.ac.id>
2024-03-08 14:43:13 +0700

    login n register sudah selesai

--- app/src/main/java/com/example/barangku/activity/Login.java
+++ app/src/main/java/com/example/barangku/activity/Login.java
@@ -18,12 +18,14 @@ import android.widget.Toast;
 import com.example.barangku.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
-    private EditText etEmail_login,etPassword_login;
+    private TextInputEditText etEmail_login,etPassword_login;
     private Button btn_login;
     private TextView tv_register;
     private FirebaseAuth mAuth;
@@ -90,27 +92,8 @@ public class Login extends AppCompatActivity {
                                }
                            }
                        });
-           } else {
-               // Display a message to the user telling them to enter an email and password
-               Toast.makeText(Login.this, "Silakan Isi Email dan Password", Toast.LENGTH_SHORT).show();
            }
-//           mAuth.signInWithEmailAndPassword(email, password)
-//                   .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
-//                       @Override
-//                       public void onComplete(@NonNull Task<AuthResult> task) {
-//                           if (task.isSuccessful()) {
-//
-//                               Toast.makeText(Login.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
-//                               Intent intent = new Intent(Login.this, MainActivity.class);
-//                               startActivity(intent);
-//                               finish();
-//                           } else {
-//
-//                               Toast.makeText(Login.this, "Authentication failed.",
-//                                       Toast.LENGTH_SHORT).show();
-//                           }
-//                       }
-//                   });
+
 
 
        }
--- app/src/main/java/com/example/barangku/activity/Register.java
+++ app/src/main/java/com/example/barangku/activity/Register.java
@@ -19,12 +19,13 @@ import android.widget.Toast;
 import com.example.barangku.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
-private EditText etregister_email,etregister_password;
+private TextInputEditText etregister_email,etregister_password;
 private Button btn_register;
 private TextView tv_login;
 private String email, password;
